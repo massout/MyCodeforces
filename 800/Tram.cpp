6c7e9c23720334c6c4e8986e5bb11b6f9fc11024
Mesut Tunçer <53650821+massout@users.noreply.github.com>
2021-12-14 13:31:58 +0300

    Create Tram.cpp

---
+++ 800/Tram.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#define ll long long int 
+
+using namespace std;
+
+int main(){
+    int a, b, c, d=0, e=0;
+
+    cin >> a;
+
+    while(a>0){
+        cin >> b >> c;
+
+        d += c-b;
+
+        if(d>e)
+            e=d;
+
+        a--;
+    }
+
+    cout << e << endl;
+
+    return 0;
+}
